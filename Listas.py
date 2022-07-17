433ca734f609c9f6172e09badb32edfdcf1bb8db
Xisco <xisco@xisco.com>
2022-07-17 19:16:24 +0200

    update Listas.py

--- Listas.py
+++ Listas.py
@@ -1,10 +1,1 @@
-hola = []
-for a in range(4):
-    hola.append([])
-    for b in range(4):
-        hola[a].append([])
-        for c in range(4):
-            hola[a][b].append(["a"])
-
-for a in hola:
-    print(a)
+hola = ["candy","caca","perro"]
