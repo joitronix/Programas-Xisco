83f2169a6a2456087c502846aa0c41b0b57a46f2
Xisco <xisco@xisco.com>
2022-07-24 19:57:12 +0200

    a

--- OrdenarNumeros.py
+++ OrdenarNumeros.py
@@ -1,19 +1,40 @@
 import random
+import os
 #Generamos lista de Numeros:
-
-Cantidad = int(input("Cantidad de Numeros a ordenar?: "))
 Numeros = []
-ResultadoOrdenado = []
-for c in range(999999999):
-    ResultadoOrdenado.append(0)
-for a in range(Cantidad):
-    NumeroAleatorio = random.randint(0,999999999)
-    Numeros.append(NumeroAleatorio)
-for b in Numeros:
-    ResultadoOrdenado.insert(b,ResultadoOrdenado[b] + 1)
+Cantidad = int(input("Cantidad de Numeros a ordenar?: "))
+for c in range(Cantidad):
+    Numeros.append(random.randint(0,999999999))
+#---------------------------------------------------------------------
 
-print(len(ResultadoOrdenado))
-print(Numeros)
-for z,f in enumerate(ResultadoOrdenado):
-    if f != 0:
-        print(f"Numero: {z} apareció: {f} veces.")
+Movidos = 0
+Iteraciones = 0
+Movimientos = 0
+Iteraciones1 = 0
+SaltosIteracion = 0
+
+for i,h in enumerate(Numeros):
+    os.system("cls")
+    print(i)
+    print(f"Cantidad de Iteraciones: {Iteraciones} : {Iteraciones1} con {SaltosIteracion} saltos de iteracion y se han movido {Movimientos}")
+    if Movidos > 0:
+        SaltosIteracion += 1
+        Movidos -= 1
+        continue
+    
+    for f in range(i+1,len(Numeros)):
+        Iteraciones += 1
+        if Numeros[i] > Numeros[f]:
+            Num1 = Numeros[i]
+            Numeros[i] = Numeros[f]
+            Numeros[f] = Num1
+
+    for z in range(i+1,len(Numeros)):
+        Iteraciones1 += 1
+        if Numeros[i] == Numeros[z]:
+            Numeros.pop(z)
+            Numeros.insert(i,Numeros[i])
+            Movidos += 1
+            Movimientos +=1
+            
+print(f"Cantidad de Iteraciones: {Iteraciones} : {Iteraciones1} con {SaltosIteracion} saltos de iteracion y se han movido {Movimientos}")
