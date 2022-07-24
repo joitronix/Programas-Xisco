0287046d31044a71d41df220b9cff90ca6846729
Xisco <xisco@xisco.com>
2022-07-24 20:55:01 +0200

    b

--- OrdenarNumerosPosicionamiento.py
+++ OrdenarNumerosPosicionamiento.py
@@ -3,22 +3,29 @@ import sys
 import os
 import random
 start_time = time.time()
-
-#Generamos lista de Numeros:
+os.system("cls")
+#Generamos lista de Numeros:--------------------------------------------------
 Numeros = []
 Cantidad = int(input("Cantidad de Numeros a ordenar?: "))
 for c in range(Cantidad):
     Numeros.append(random.randint(0,10))
-#---------------------------------------------------------------------
-#Buscamos el numero mas grande:
+#-----------------------------------------------------------------------------
+#Buscamos el numero mas grande:-----------------------------------------------
 NumMasGrande = 0
 for c in Numeros:
     if c > NumMasGrande:
         NumMasGrande = c
 #-----------------------------------------------------------------------------
+#Generamos lista de posicionamiento de numeros:-------------------------------
 candy = []
-for a in range(NumMasGrande):
+print(f"El numero mas grande es: {NumMasGrande}")
+for a in range(NumMasGrande + 1):
     candy.append(0)
-
+#-----------------------------------------------------------------------------
+#Asignamos los numeros a sus respectivas posiciones:--------------------------
+for z in Numeros:
+    candy[z] = candy[z] + 1
+#-----------------------------------------------------------------------------
+print(candy)
 print("--- %s seconds ---" % (time.time() - start_time))
 print(f"El numero mas grande es {NumMasGrande}")
