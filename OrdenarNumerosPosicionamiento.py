c6c65484cd53b04e315911402eecf76552f32b56
Xisco <xisco@xisco.com>
2022-07-24 21:10:16 +0200

    BorramosElAlgoritmo

--- OrdenarNumerosPosicionamiento.py
+++ OrdenarNumerosPosicionamiento.py
@@ -6,9 +6,10 @@ start_time = time.time()
 os.system("cls")
 #Generamos lista de Numeros:--------------------------------------------------
 Numeros = []
+Resultado = []
 Cantidad = int(input("Cantidad de Numeros a ordenar?: "))
 for c in range(Cantidad):
-    Numeros.append(random.randint(0,10))
+    Numeros.append(random.randint(0,100000))
 #-----------------------------------------------------------------------------
 #Buscamos el numero mas grande:-----------------------------------------------
 NumMasGrande = 0
@@ -26,6 +27,10 @@ for a in range(NumMasGrande + 1):
 for z in Numeros:
     candy[z] = candy[z] + 1
 #-----------------------------------------------------------------------------
-print(candy)
+#Generamos lista Ordenada:----------------------------------------------------
+for i,a in enumerate(candy):
+    for b in range(a):
+        Resultado.append(i)
+#-----------------------------------------------------------------------------
+print(Resultado)
 print("--- %s seconds ---" % (time.time() - start_time))
-print(f"El numero mas grande es {NumMasGrande}")
