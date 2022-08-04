ecffaed110051416bb1513e750118604fc03f07c
Xisco <xisco@xisco.com>
2022-08-04 22:12:58 +0200

    testing

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,4 +1,4 @@
 import Funciones
 
-print(Funciones.Multiplicacion(3,3,3,3,3,3,3))
+print(Funciones.Multiplicacion(3,3,3,3,3,3,53))
 
--- Funciones.py
+++ Funciones.py
@@ -1,11 +1,16 @@
+#=====================================================================================================
 def Suma(*args):
     return sum(args)
+#-----------------------------------------------------------------------------------------------------
+
+#=====================================================================================================
 def Multiplicacion(*args):
     Result = ""
-    for Index,Numbers in enumerate(args):
-        if Index == (len(args) - 1):
-            Result += f"{Numbers}"
-            continue
-        Result += f"{Numbers} * "
+    #Range le quitamos uno para que no muestre el ultimo digito(por defecto ya quita el extra de len)
+    for IndexNumber in range(0,len(args) - 1):
+        Result += f"{args[IndexNumber]} * "
+    #Mostramos el ultimo valor que queriamos sin el *.
+    Result += str(args[len(args) - 1])
             
     return eval(Result)
+#-----------------------------------------------------------------------------------------------------
