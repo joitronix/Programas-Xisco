62cc9b6a411324b149f8e01a0e1673f907b70318
Xisco <xisco@xisco.com>
2022-08-08 18:29:19 +0200

    f

--- POKEMON/FuncionesPokemon.py
+++ POKEMON/FuncionesPokemon.py
@@ -5,6 +5,7 @@ def TextoImpresoLento(Texto,velocidad):
         print( g , end='' , flush=True )
         time.sleep(velocidad)
 #-----------------------------------------------------------------------------------------------------
+#=====================================================================================================
 def QuitarVida(Vida,Cantidad):
     Vida = list(Vida)
     for z,a in enumerate(Vida[::-1]):
@@ -14,3 +15,15 @@ def QuitarVida(Vida,Cantidad):
         if(Cantidad == 0):
             break
     return str(''.join(Vida))
+#-----------------------------------------------------------------------------------------------------
+#=====================================================================================================
+def QuitarVida(Vida,Cantidad):
+    Vida = list(Vida)
+    for z,a in enumerate(Vida[::-1]):
+        if a == '=':
+            Vida[len(Vida) - 1 - z] = " "
+            Cantidad -= 1
+        if(Cantidad == 0):
+            break
+    return str(''.join(Vida))
+#-----------------------------------------------------------------------------------------------------
