5072980e5e6d26a96e65e5f2d0653e30a6941e2b
Xisco <xisco@xisco.com>
2022-08-08 18:19:00 +0200

    i

--- POKEMON/Pokemon.py
+++ POKEMON/Pokemon.py
@@ -3,22 +3,23 @@ import FuncionesPokemon
 POKEMON1 = "POKEMON" + "   ||========================================================||"
 POKEMON2 = "POKEMON" + "   ||========================================================||"
 while True:
+    
     print(POKEMON1)
-    print("Bonificacion de ataque: 33")
-    print("Bonificacion de velocidad: 43")
-    print("Bonificacion de defensa: 23")
+    print(f"Bonificacion de ataque: 33")
+    print(f"Bonificacion de velocidad: 43")
+    print(f"Bonificacion de defensa: 23")
     
     print(POKEMON2)
-    print("Bonificacion de ataque: 21")
-    print("Bonificacion de velocidad: 12")
-    print("Bonificacion de defensa: 18")
+    print(f"Bonificacion de ataque: 21")
+    print(f"Bonificacion de velocidad: 12")
+    print(f"Bonificacion de defensa: 18")
     
-    print("1.Impactrueno: 5pd")
+    
     Ataque = input("Elija el ataque seleccionado: ")
     if Ataque == '1':
         POKEMON1 = FuncionesPokemon.QuitarVida(POKEMON1,5)
         
     if '=' not in POKEMON1 or '=' not in POKEMON2:
-        print("Combate Terminado.")
+        FuncionesPokemon.TextoImpresoLento("Combate Finalizado.",0.03)
         break 
     
