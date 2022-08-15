94bfdefb6682a511a1765aede1dac246e35d9803
Xisco <xisco@xisco.com>
2022-08-15 02:15:27 +0200

    Lol db txt

--- ARITMETICA/Aritmetica.py
+++ ARITMETICA/Aritmetica.py
@@ -1,13 +1,13 @@
-class Aritmetica:
-    list = []
-    def __init__(self,hola):
-        self.hola = hola
-        Aritmetica.list.append(self)
-
-for a in range(0,4):
-    Aritmetica(a)
-
-for a in Aritmetica.list:
-    print(a.hola)
+class Rectangulo:
+    def __init__(self,altura,anchura) -> None:
+        self.anchura = anchura
+        self.altura = altura
+    
+    def CalcularArea(self):
+        return int(self.altura) * int(self.anchura)
+    
+anchura = input("Proporciona la base: ")
+altura = input("Proporciona la altura: ")
+print(f"El area del rectangulo es: {Rectangulo(anchura,altura).CalcularArea()}")
 
 
---
+++ PROYECTO DB/Funciones.py
@@ -0,0 +1,62 @@
+import os.path
+import os
+from tkinter import SE
+
+def CrearTabla(Nombre,Columnas):
+    try:
+        with open(f'{Nombre}.txt', 'x') as f:
+            f.write('º'.join(str(x) for x in Columnas) + "\n")
+        print("Tabla creada correctamente")
+    except:
+        print("La tabla ya existe")
+        
+def SelectColumnasTabla(Tabla):
+    if os.path.exists(f'{Tabla}.txt'):
+        with open(f"{Tabla}.txt", "r") as file:
+            return file.readline().strip().split("º")
+
+def InserTabla(Tabla,ListaInsert):
+    try:
+        if os.path.exists(f'{Tabla}.txt'):
+            if len(SelectColumnasTabla(Tabla)) == len(ListaInsert):
+                with open(f'{Tabla}.txt', 'a+') as f:
+                    f.write( 'º'.join(str(x) for x in ListaInsert) + "\n")
+                print("Elemento insertado correctamente")
+            else:
+                print("Numero de argumentos no valido")
+        else:
+            print(f"La tabla {Tabla} no existe.")
+    except:
+        print("Error Insertando.")
+
+CrearTabla("Clientes",["Nombre","Apellido","TipoDeSangre"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+InserTabla("Clientes",["Tomeu","Pandero","a-Negativo"])
+
